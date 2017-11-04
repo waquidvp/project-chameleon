11cde46ab598123e0cd3f8a6433b19f873ceea3c
Waquid Valiya Peedikakkal <waquidvp@gmail.com>
2017-11-04 19:28:36 +0000

    Completed server side dev setup

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "extends": "airbnb",
+    "rules": {
+        "react/jsx-filename-extension": [1, { 
+          "extensions": [".js", ".jsx"] 
+        }],
+        "react/require-default-props": [0],
+        "react/no-unused-prop-types": [2, {
+            "skipShapeProps": true
+        }],
+        "react/no-multi-comp": [0],
+        "no-bitwise": [0],
+    },
+};
