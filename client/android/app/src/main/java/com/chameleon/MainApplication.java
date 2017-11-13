801280819d94a3ac05d07b1a6bf3a2f042db7487
Waquid Valiya Peedikakkal <waquidvp@gmail.com>
2017-11-13 22:21:18 +0000

    Added facebook sdk - setup android

--- client/android/app/src/main/java/com/chameleon/MainApplication.java
+++ client/android/app/src/main/java/com/chameleon/MainApplication.java
@@ -3,16 +3,26 @@ package com.chameleon;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.facebook.FacebookSdk;
+import com.facebook.CallbackManager;
+import com.facebook.appevents.AppEventsLogger;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+  
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -24,7 +34,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RCTCameraPackage()
+            new RCTCameraPackage(),
+            new FBSDKPackage(mCallbackManager)
       );
     }
 
