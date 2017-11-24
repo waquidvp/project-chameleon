48f168f47a930d67cf51795ea8f0fb439604d2cf
Waquid Valiya Peedikakkal <waquidvp@gmail.com>
2017-11-24 22:52:30 +0000

    Added error middleware

--- client/src/api/gql-client.js
+++ client/src/api/gql-client.js
@@ -4,6 +4,8 @@ import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context';
+import { onError } from 'apollo-link-error';
+import { from } from 'apollo-link';
 
 import { getToken } from '../utils/token';
 
@@ -11,7 +13,11 @@ export const GRAPHQL_ENDPOINT_URI = 'http://10.0.2.2:8080/graphql';
 
 const httpLink = new HttpLink({ uri: GRAPHQL_ENDPOINT_URI });
 
-const authLink = setContext(async (req, { headers }) => {
+const errorLink = onError((error) => {
+  console.warn(error);
+});
+
+const authMiddleware = setContext(async (req, { headers }) => {
   const token = await getToken();
 
   if (token) {
@@ -28,7 +34,11 @@ const authLink = setContext(async (req, { headers }) => {
   };
 });
 
-const link = authLink.concat(httpLink);
+const link = from([
+  errorLink,
+  authMiddleware,
+  httpLink,
+]);
 
 const client = new ApolloClient({
   link,
