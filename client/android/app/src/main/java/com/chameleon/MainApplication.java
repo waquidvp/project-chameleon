39cdda1e824295e5ccc255fe8b6afb6401c4175b
Archie Godfrey <archiegodfrey@Archies-MacBook-Pro.local>
2017-12-02 09:06:54 +0000

    Added react-native-blur

--- client/android/app/src/main/java/com/chameleon/MainApplication.java
+++ client/android/app/src/main/java/com/chameleon/MainApplication.java
@@ -3,6 +3,7 @@ package com.chameleon;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.cmcewen.blurview.BlurViewPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactNativeHost;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new BlurViewPackage(),
             new RCTCameraPackage(),
             new FBSDKPackage(mCallbackManager)
       );
