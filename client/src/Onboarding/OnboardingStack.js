2d038b8060ec103800724479d31927cb0f8a1aa2
Waquid Valiya Peedikakkal <waquidvp@gmail.com>
2017-12-17 20:17:09 +0000

    Added basic template for confirm, register, and forgot password screens

--- client/src/Onboarding/OnboardingStack.js
+++ client/src/Onboarding/OnboardingStack.js
@@ -3,15 +3,30 @@
 import { StackNavigator } from 'react-navigation';
 
 import Login from './Login';
+import Register from './Register';
+import Confirm from './Confirm';
+import ForgotPassword from './ForgotPassword';
 
-const OnboardingStack = StackNavigator({
-  Login: {
-    screen: Login,
+const OnboardingStack = StackNavigator(
+  {
+    Login: {
+      screen: Login,
+    },
+    Register: {
+      screen: Register,
+    },
+    Confirm: {
+      screen: Confirm,
+    },
+    ForgotPassword: {
+      screen: ForgotPassword,
+    },
   },
-}, {
-  navigationOptions: {
-    header: null,
+  {
+    navigationOptions: {
+      header: null,
+    },
   },
-});
+);
 
 export default OnboardingStack;
---
+++ client/src/Onboarding/Register.js
@@ -0,0 +1,17 @@
+// @flow
+
+import React, { Component } from 'react';
+import styled from 'styled-components/native';
+
+const MainContainer = styled.View`
+  flex: 1;
+`;
+
+class Register extends Component {
+  state = {};
+  render() {
+    return <MainContainer />;
+  }
+}
+
+export default Register;
