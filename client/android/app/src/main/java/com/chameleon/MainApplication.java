e8e3296cf39d9854dbf7ba025a8bc856421dc9b9
Waquid Valiya Peedikakkal <waquidvp@gmail.com>
2017-12-21 23:08:38 +0000

    Added react-native-interactable

--- client/android/app/src/main/java/com/chameleon/MainApplication.java
+++ client/android/app/src/main/java/com/chameleon/MainApplication.java
@@ -3,6 +3,7 @@ package com.chameleon;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.wix.interactable.Interactable;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.cmcewen.blurview.BlurViewPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
@@ -36,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new Interactable(),
             new LinearGradientPackage(),
             new BlurViewPackage(),
             new RCTCameraPackage(),
