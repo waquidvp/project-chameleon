1279e95132889458725d952215698395bb1f5261
Waquid Valiya Peedikakkal <waquidvp@gmail.com>
2017-12-24 11:34:48 +0000

    Removed Apollo temporarily

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 // @flow
 
 import React from 'react';
-import { ApolloProvider } from 'react-apollo';
+// import { ApolloProvider } from 'react-apollo';
 
-import client from './api/gql-client';
+// import client from './api/gql-client';
 import Onboarding from './Onboarding/Main';
 import Home from './Home/Home';
 import { signIn, signOut, getToken } from './utils/token';
@@ -37,7 +37,7 @@ export default class App extends React.Component {
   render() {
     if (this.state.render) {
       return (
-        <ApolloProvider client={client}>
+        {/* <ApolloProvider client={client}> */}
           {/* {this.state.loggedIn ? ( */}
           <Home
             screenProps={{ changeLoginState: this.handleChangeLoginState }}
@@ -45,7 +45,7 @@ export default class App extends React.Component {
           {/* ) : ( */}
           {/* <Onboarding changeLoginState={this.handleChangeLoginState} /> */}
           {/* )} */}
-        </ApolloProvider>
+        {/* </ApolloProvider> */}
       );
     }
 
